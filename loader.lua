056a00384fdbc6a2e19394bf422fea7ac1fdc35a
r1FH <91796669+r1FH@users.noreply.github.com>
2023-02-03 13:17:21 +0100

    loader.lua

---
+++ loader.lua
@@ -0,0 +1,91 @@
+--IMG Importer has been combined with the Key System GT.
+
+--I highly suggest you dont change the settings.
+
+--[[
+
+                  _ _   
+
+                 | | |  
+
+__   ____ _ _   _| | |_ 
+
+\ \ / / _` | | | | | __|
+
+ \ V / (_| | |_| | | |_ 
+
+  \_/ \__,_|\__,_|_|\__|
+
+                        
+
+                        
+
+]]
+
+ 
+
+ getgenv().ImageImporterSettings = {
+
+    
+
+    PrimaryPartUse = true,
+
+    AutoBitrate = false,
+
+    Anti_F3X_CC = true,
+
+    ALT_Bitrate = true,
+
+    AdvChecker = true,
+
+    JointChecker = true,
+
+    BuildSpread = true,
+
+    Universal_ZJ = true,
+
+    AntiDelete = true,
+
+    Enabled = true,
+
+    ShowDetect = false,
+
+    character_Check = true,
+
+    K_O_Check = true,
+
+    FWIW_Checker = true,
+
+    Anti3D = false,
+
+    Anti_V2 = false,
+
+    Intact = true,
+
+    Build_Filled = false,
+
+    Nearest_Player = true,
+
+    Custom_Bitrate = false,
+
+    Custom_Checker = true,
+
+    Auto_ChangeBITRATE = true,
+
+    UseAPI = false,
+
+    Mass = false,
+
+    Auto_Change_Bit_Rate = {"10","20","30","40","50","60"},
+
+    BitratePatch = {
+
+        "BitDefender",
+
+        "BitChecker"
+
+    }
+
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/r1FH/OBF-Unbypassed-Key-System/main/main.lua", true))()
